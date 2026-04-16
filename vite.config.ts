2038b02dc6f73bcaef5e2c0804f95be154960928
manaux <litterball@gmail.com>
2026-04-16 09:59:56 -0400

    chore(deps): add motion and vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   base: '/HW4-checker/',
   test: {
     globals: true,
